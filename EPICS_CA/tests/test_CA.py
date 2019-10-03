ee2afa8a05451ebd17d4d9baccaf1453367d056e
Thomas A Caswell <tcaswell@bnl.gov>
2019-10-02 21:12:14 -0700

    STY: apply black

--- EPICS_CA/tests/test_CA.py
+++ EPICS_CA/tests/test_CA.py
@@ -1,19 +1,22 @@
 def test_connect():
     """Check that casput and caget work together"""
-    PV_name = "TEST:TEST.VAL" 
+    PV_name = "TEST:TEST.VAL"
     from ..CAServer import casput
-    casput(PV_name,1)
+
+    casput(PV_name, 1)
     from ..CA import caget
+
     assert caget(PV_name) == 1
 
-def _test_disconnect():
+
+def test_disconnect():
     """Check that 'casdel' disconnects a PV"""
-    from ..CAServer import casput,casget,casdel
+    from ..CAServer import casput, casget, casdel
     from ..CA import caget
     from time import sleep
 
-    PV_name = "TEST:TEST.VAL" 
-    casput(PV_name,1)
+    PV_name = "TEST:TEST.VAL"
+    casput(PV_name, 1)
     assert casget(PV_name) == 1
     assert caget(PV_name) == 1
     casdel(PV_name)
